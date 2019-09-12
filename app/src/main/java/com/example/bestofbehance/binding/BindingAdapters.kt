bd354ef20512eb575827a70aa471e96d8b9023b6
Deltenman <subros.lxi@gmail.com>
2019-09-12 12:56:35 +0300

    Ничего существенного, только небольшие исправления в xml-файлах [3] + заменен Glide на GlideApp в BindingAdapter'ах

--- app/src/main/java/com/example/bestofbehance/binding/BindingAdapters.kt
+++ app/src/main/java/com/example/bestofbehance/binding/BindingAdapters.kt
@@ -1,43 +1,40 @@
 package com.example.bestofbehance.binding
 
-import android.graphics.Bitmap
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
-import com.bumptech.glide.load.Transformation
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.RequestOptions.bitmapTransform
+import com.example.bestofbehance.viewModels.GlideApp
+import com.example.bestofbehance.viewModels.GlideOptions.circleCropTransform
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation
-import org.jetbrains.anko.matchParent
 
 object BindingAdapters{
     @JvmStatic
     @BindingAdapter("loadingImage")
     fun setImageUrl(view: ImageView, url: String?) {
-        Glide.with(view.context).load(url).into(view)
+        GlideApp.with(view.context).load(url).into(view)
     }
 
     @JvmStatic
     @BindingAdapter(value = ["loadingRoundedImage", "loadingThumbnail"])
     fun setRoundedImageUrl(view: ImageView, url: String?, turl: String?) {
         val options = RequestOptions().transform(MultiTransformation(CenterCrop(), RoundedCornersTransformation(15, 0)))
-        Glide.with(view.context).load(url).thumbnail(Glide.with(view.context).load(turl).diskCacheStrategy(DiskCacheStrategy.ALL).apply(RequestOptions.centerCropTransform())).apply(options).into(view)
+        GlideApp.with(view.context).load(url).thumbnail(Glide.with(view.context).load(turl).apply(options)).apply(options).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(view)
     }
 
     @JvmStatic
     @BindingAdapter("loadingCircleImage")
     fun setCircleImageUrl(view: ImageView, url: String?) {
-        Glide.with(view.context).load(url).apply(RequestOptions.circleCropTransform()).into(view)
+        GlideApp.with(view.context).load(url).apply(circleCropTransform()).into(view)
     }
 
     @JvmStatic
     @BindingAdapter(value = ["loadingBigCircleImage", "loadingThumbnail"])
     fun setBigCircleImageUrl(view: ImageView, url: String?, turl: String?) {
-        Glide.with(view.context).load(url).thumbnail(Glide.with(view.context).load(turl).apply(RequestOptions.circleCropTransform())).apply(RequestOptions.circleCropTransform()).into(view)
+        GlideApp.with(view.context).load(url).thumbnail(Glide.with(view.context).load(turl).apply(circleCropTransform())).apply(circleCropTransform()).into(view)
     }
 
 }
